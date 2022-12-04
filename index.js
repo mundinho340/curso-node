6754057c08449216b50c87d40ec645ce32d5a5fc
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-04 16:22:02 -0200

    conectando com a base de dados

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('teste', 'root','',{
+    host: 'localhost',
+    dialect:'mysql'
+})
 
 //config
 //template Engine
