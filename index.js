5181e3ce92a5aebd28fbaa2aee4d9d8975778bd6
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-04 16:23:58 -0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('teste', 'root','',{
-    host: 'localhost',
-    dialect:'mysql'
-})
 
 //config
 //template Engine
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.stack('view engine', 'handlebars')
+    //conexao com o banco de dados sql
+    const sequelize = new Sequelize('teste', 'root','',{
+        host: 'localhost',
+        dialect:'mysql'
+    })
+    
 app.listen(8081, function(){
     console.log('Servidor Rodando na url https://localhost:8081')
 })
