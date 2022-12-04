fa6ee47ee2988d17718c1aaa6b9cc3e06422aba4
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-04 15:50:28 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+
+//config
+//template Engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.stack('view engine', 'handlebars')
