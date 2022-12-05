5358a4a27417b852b7bee2bdb0dc7c3ba5a217f5
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-05 01:09:05 -0200

    formulario

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ var handle = exphbs.create({
     })
     //Rotas
     app.get('/cad', function(req, res){
-        res.render('formulario.handlebars')
-    })
+        res.render('formulario')
+        })
 app.listen(8081, function(){
     console.log('Servidor Rodando na url https://localhost:8081')
 })
