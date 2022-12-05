f688038d90d9c9259948ca80d641afe1d66d76be
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-04 23:59:31 -0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ var handle = exphbs.create({
         dialect:'mysql'
     })
     //Rotas
- 
+    app.get('/', function(req, res){
+        res.send('pagina vasia')
+    })
+    app.get('/cad', function(req, res){
+        res.send('Ola Raimundo o servidor esta rodando ')
+    })
 app.listen(8081, function(){
-    //console.log('Servidor Rodando na url https://localhost:8081')
+    console.log('Servidor Rodando na url https://localhost:8081')
 })
