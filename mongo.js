52b8d9095e8e2678778d23042401dc24b77565f7
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-12 19:32:40 -0200

    mongo

--- mongo.js
+++ mongo.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost/aprendendo").then(()=> {
     console.log("MongoDB conectado ...").cacth((e)=>{
-        
+        console.log("Houve um erro ao se connectar ao Mongo "+e)
     })
 })
