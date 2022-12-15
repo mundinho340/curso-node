f28a47dad5d2970613d1eab5ce0548d5a0fa9f48
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-14 23:56:25 -0200

    mongo_connection

--- executavel.js
+++ executavel.js
@@ -1,15 +1,12 @@
 const mongoose =require('mongoose')
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://localhost:27017/bancoTeste").then(()=>{
 
+mongoose.connect("mongodb://127.0.0.1:27017/bancoTeste").then(()=>{
  console.log("Banco conectado com sucesso!!!")
-
-
-
 }).catch((err)=>{
 
-console.log("Houve um erro ao se conectar ao banco:" +err)
+    console.log("Houve um erro ao se conectar ao banco:" +err)
 
 
 
